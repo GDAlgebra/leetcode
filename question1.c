afc680ec6f32ede6db8ab8c7a6d0d2bfed53950d
GDAlgebra <93385652+GDAlgebra@users.noreply.github.com>
2022-01-18 22:16:09 +0800

    Create question1.c

---
+++ question1.c
@@ -0,0 +1,22 @@
+/**
+ * question1:Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *a = (int*)malloc(sizeof(int)*2); 
+	for (int i = 0; i < numsSize - 1; i++){
+		for (int j = i+1; j < numsSize; j++){
+			if (nums[i] + nums[j] == target)
+			{
+				a[0] = i;
+				a[1] = j;
+                *returnSize = 2;
+				return a;
+			}
+		}
+	}
+    *returnSize = 0;
+	return 0;
+}
