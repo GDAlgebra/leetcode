48f8829dc362228da8e65093f57619eae85eaf72
GDAlgebra <93385652+GDAlgebra@users.noreply.github.com>
2022-03-15 20:36:34 +0800

    Create question20.c

---
+++ question20.c
@@ -0,0 +1,32 @@
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * An input string is valid if:
+ * 1.Open brackets must be closed by the same type of brackets.
+ * 2.Open brackets must be closed in the correct order.
+ */
+char pairs(char a) {
+    if (a == '}') return '{';
+    if (a == ']') return '[';
+    if (a == ')') return '(';
+    return 0;
+}
+
+bool isValid(char* s) {
+    int n = strlen(s);
+    if (n % 2 == 1) {
+        return false;
+    }
+    int stk[n + 1], top = 0;
+    for (int i = 0; i < n; i++) {
+        char ch = pairs(s[i]);
+        if (ch) {
+            if (top == 0 || stk[top - 1] != ch) {
+                return false;
+            }
+            top--;
+        } else {
+            stk[top++] = s[i];
+        }
+    }
+    return top == 0;
+}
